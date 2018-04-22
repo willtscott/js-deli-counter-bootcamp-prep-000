81464113293bb926f0d669bf6874dc1908b57d7f
wtscott@gmail.com <wtscott@gmail.com>
2018-04-22 00:57:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name)
-  return 'Welcome, Ada. You are number #{katzDeliLine.length} in line.' }
+  katzDeliLine.push(name);
+  return "Welcome, Ada. You are number "+katzDeliLine.length+" in line." }
   
+function nowServing(line) {
+  if(line.length<1) { 
+    return "There is nobody waiting to be served!" }
+  else {
+    return "Currently serving " + line.shift() + "." }
+} 
